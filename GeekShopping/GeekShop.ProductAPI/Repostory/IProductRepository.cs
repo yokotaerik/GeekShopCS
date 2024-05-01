ad7b5874f8434b78e86fc569e29b980f3018cb83
yokotaerik <yokota082003@gmail.com>
2024-05-01 18:59:48 -0300

    feat: Criando a Interface do repositorio de produto

--- GeekShopping/GeekShop.ProductAPI/Repostory/IProductRepository.cs
+++ GeekShopping/GeekShop.ProductAPI/Repostory/IProductRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace GeekShop.ProductAPI.Repostory
+﻿using GeekShop.ProductAPI.Data.ValueObjects;
+using GeekShop.ProductAPI.Model;
+
+namespace GeekShop.ProductAPI.Repostory
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<bool> Delete(long id);
     }
 }
